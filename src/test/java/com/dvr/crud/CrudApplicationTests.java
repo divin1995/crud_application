d7b65334168f1d1f9ee2eb7386f0e86767ace98c
Divin Vishnu Ramachandran <divinvishnu@gmail.com>
2020-07-16 19:31:05 -0500

    Hello World RESTful API on Spring Boot

---
+++ src/test/java/com/dvr/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvr.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
